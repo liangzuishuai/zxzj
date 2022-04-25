a033ad351e66cf7afab74a135f258c029142eba8
Remember Me <2981941461@qq.com>
2022-04-25 15:29:57 +0800

    1111

---
+++ pbtrade/precache-manifest.f2f2527b14bcab8fe1f85a248e5ea98f.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f89464e60769385f4d0",
+    "url": "/pbtrade/css/app.a23d874d.css"
+  },
+  {
+    "revision": "902d5896bdba1f522f63",
+    "url": "/pbtrade/css/chunk-0ef18d3a.ee74ce29.css"
+  },
+  {
+    "revision": "f092de42a7660e332efd",
+    "url": "/pbtrade/css/chunk-179c8d90.5ae0b7ed.css"
+  },
+  {
+    "revision": "779a73a76c96e302cd1d",
+    "url": "/pbtrade/css/chunk-1ae21c4e.7d980f17.css"
+  },
+  {
+    "revision": "1a7b485ca1620ba1c5ec",
+    "url": "/pbtrade/css/chunk-1e3de27a.8c9ef1af.css"
+  },
+  {
+    "revision": "f1e864e623d411ca81cc",
+    "url": "/pbtrade/css/chunk-1f1bb868.676b42f6.css"
+  },
+  {
+    "revision": "a84d1e176d788c5b81e9",
+    "url": "/pbtrade/css/chunk-29b9f620.c1e0cf53.css"
+  },
+  {
+    "revision": "8fb91f95c89701da8f17",
+    "url": "/pbtrade/css/chunk-3436cc6b.050a8d41.css"
+  },
+  {
+    "revision": "9a1675818bfb5888569e",
+    "url": "/pbtrade/css/chunk-394da624.c185517b.css"
+  },
+  {
+    "revision": "2439c06ac75de2e03aa6",
+    "url": "/pbtrade/css/chunk-42dd2fb2.c425b11f.css"
+  },
+  {
+    "revision": "228cbcfc68369b61068b",
+    "url": "/pbtrade/css/chunk-481abeb8.5438a510.css"
+  },
+  {
+    "revision": "e90cd77912a0921696a4",
+    "url": "/pbtrade/css/chunk-4ccbfcfc.3fe8ff14.css"
+  },
+  {
+    "revision": "ca4179e9a9a9a49bce57",
+    "url": "/pbtrade/css/chunk-54e68dfa.7bbf76f0.css"
+  },
+  {
+    "revision": "39196ba96098e649be22",
+    "url": "/pbtrade/css/chunk-6aec86d0.7add8cf8.css"
+  },
+  {
+    "revision": "e2bdbf79706f1ba2cbd1",
+    "url": "/pbtrade/css/chunk-6d96bc00.5382b082.css"
+  },
+  {
+    "revision": "14b0295cc7242015c52d",
+    "url": "/pbtrade/css/chunk-77cecfec.581ae0ba.css"
+  },
+  {
+    "revision": "5d87494baba78f9763af",
+    "url": "/pbtrade/css/chunk-957c56ae.719d4b93.css"
+  },
+  {
+    "revision": "a32acf0ed175766f470d",
+    "url": "/pbtrade/css/chunk-a74ab698.693199c2.css"
+  },
+  {
+    "revision": "4befaed19c5294df773d",
+    "url": "/pbtrade/css/chunk-b12aaa7e.88d42454.css"
+  },
+  {
+    "revision": "3a0348b5218c66496e8a",
+    "url": "/pbtrade/css/chunk-cd45a8dc.ea0ec38c.css"
+  },
+  {
+    "revision": "b50d3a9a3b10080b116a",
+    "url": "/pbtrade/css/chunk-vendors.a16c4353.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/pbtrade/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/pbtrade/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "7378e04a3381603cdca8442ca7a85817",
+    "url": "/pbtrade/img/bj_img4.7378e04a.png"
+  },
+  {
+    "revision": "0c028d9264f9295571ee318b19b18be4",
+    "url": "/pbtrade/img/bjimg2.0c028d92.png"
+  },
+  {
+    "revision": "80e3f7e14097967160a0642aa540f34b",
+    "url": "/pbtrade/img/icon_code.80e3f7e1.png"
+  },
+  {
+    "revision": "f169ad81debaa5d733897c944b6c7c68",
+    "url": "/pbtrade/img/icon_code2.f169ad81.png"
+  },
+  {
+    "revision": "08d11dd0c4535c667067eb4d72282ed9",
+    "url": "/pbtrade/img/index_img1.08d11dd0.png"
+  },
+  {
+    "revision": "8f9f0ea9792fa12ff87dd5da035c1a1c",
+    "url": "/pbtrade/img/index_img11.8f9f0ea9.png"
+  },
+  {
+    "revision": "bd328c6e15857830f06f84d6724e25ad",
+    "url": "/pbtrade/img/infor_img1.bd328c6e.png"
+  },
+  {
+    "revision": "45732f24db35b30e19a8ad99c85d5f50",
+    "url": "/pbtrade/img/logo.45732f24.png"
+  },
+  {
+    "revision": "b744b0acee7136bd2f83bf7c220eab74",
+    "url": "/pbtrade/index.html"
+  },
+  {
+    "revision": "7f89464e60769385f4d0",
+    "url": "/pbtrade/js/app.9f433dde.js"
+  },
+  {
+    "revision": "902d5896bdba1f522f63",
+    "url": "/pbtrade/js/chunk-0ef18d3a.7e0b9481.js"
+  },
+  {
+    "revision": "f092de42a7660e332efd",
+    "url": "/pbtrade/js/chunk-179c8d90.4e52a40a.js"
+  },
+  {
+    "revision": "779a73a76c96e302cd1d",
+    "url": "/pbtrade/js/chunk-1ae21c4e.729594b9.js"
+  },
+  {
+    "revision": "1a7b485ca1620ba1c5ec",
+    "url": "/pbtrade/js/chunk-1e3de27a.f3987d2f.js"
+  },
+  {
+    "revision": "f1e864e623d411ca81cc",
+    "url": "/pbtrade/js/chunk-1f1bb868.4493a71f.js"
+  },
+  {
+    "revision": "a84d1e176d788c5b81e9",
+    "url": "/pbtrade/js/chunk-29b9f620.bf20be3e.js"
+  },
+  {
+    "revision": "8fb91f95c89701da8f17",
+    "url": "/pbtrade/js/chunk-3436cc6b.8036c036.js"
+  },
+  {
+    "revision": "9a1675818bfb5888569e",
+    "url": "/pbtrade/js/chunk-394da624.712964c5.js"
+  },
+  {
+    "revision": "7c71cbfb6f9513d3097c",
+    "url": "/pbtrade/js/chunk-3c2bbb8e.1239a182.js"
+  },
+  {
+    "revision": "2439c06ac75de2e03aa6",
+    "url": "/pbtrade/js/chunk-42dd2fb2.ac0b26a6.js"
+  },
+  {
+    "revision": "228cbcfc68369b61068b",
+    "url": "/pbtrade/js/chunk-481abeb8.bab2f10a.js"
+  },
+  {
+    "revision": "e90cd77912a0921696a4",
+    "url": "/pbtrade/js/chunk-4ccbfcfc.c3181e9e.js"
+  },
+  {
+    "revision": "ca4179e9a9a9a49bce57",
+    "url": "/pbtrade/js/chunk-54e68dfa.86624f0f.js"
+  },
+  {
+    "revision": "39196ba96098e649be22",
+    "url": "/pbtrade/js/chunk-6aec86d0.0799ec42.js"
+  },
+  {
+    "revision": "e2bdbf79706f1ba2cbd1",
+    "url": "/pbtrade/js/chunk-6d96bc00.8acc39d9.js"
+  },
+  {
+    "revision": "14b0295cc7242015c52d",
+    "url": "/pbtrade/js/chunk-77cecfec.596fc7d3.js"
+  },
+  {
+    "revision": "ad1d60fa1452bf2d053e",
+    "url": "/pbtrade/js/chunk-784fc681.c844b393.js"
+  },
+  {
+    "revision": "5d87494baba78f9763af",
+    "url": "/pbtrade/js/chunk-957c56ae.10ec1262.js"
+  },
+  {
+    "revision": "191868af494c2ba98834",
+    "url": "/pbtrade/js/chunk-9ad2c1c2.42263688.js"
+  },
+  {
+    "revision": "a32acf0ed175766f470d",
+    "url": "/pbtrade/js/chunk-a74ab698.611a27d4.js"
+  },
+  {
+    "revision": "4befaed19c5294df773d",
+    "url": "/pbtrade/js/chunk-b12aaa7e.d2ce5463.js"
+  },
+  {
+    "revision": "3a0348b5218c66496e8a",
+    "url": "/pbtrade/js/chunk-cd45a8dc.00a8756d.js"
+  },
+  {
+    "revision": "b50d3a9a3b10080b116a",
+    "url": "/pbtrade/js/chunk-vendors.5bfe7026.js"
+  },
+  {
+    "revision": "d031758c0b04adcca4e1578f1c9ea1b8",
+    "url": "/pbtrade/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pbtrade/robots.txt"
+  }
+]);
